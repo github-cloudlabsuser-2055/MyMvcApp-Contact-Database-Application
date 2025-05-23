9adf142a37068ce0d45e550cc451606092dcdd07
github_cloudlabsuser_2055 <github_cloudlabsuser_2055@cloudlabsaiuser.com>
2025-05-23 19:30:59 +0000

    deployazure action

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89550be0663b74536f896cb6fb0fae24aa4ea680")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e484dc9cb6648120f7675404a1aa80650da935")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
